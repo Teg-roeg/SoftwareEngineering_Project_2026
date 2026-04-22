ff63b76b0101a4fe61dc5f91463055f26017cd47
StudentGeorgeTolkach <StudentGeorgeTolkach@S304-KY-CF4L5Y3>
2026-04-22 09:22:10 +0100

    Update 2

--- CarServiceAdministration/RemoveMechanic.Designer.cs
+++ CarServiceAdministration/RemoveMechanic.Designer.cs
@@ -74,9 +74,9 @@
             this.mechBox.Controls.Add(this.MechID);
             this.mechBox.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.857143F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.mechBox.Location = new System.Drawing.Point(13, 46);
-            this.mechBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.mechBox.Margin = new System.Windows.Forms.Padding(2);
             this.mechBox.Name = "mechBox";
-            this.mechBox.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.mechBox.Padding = new System.Windows.Forms.Padding(2);
             this.mechBox.Size = new System.Drawing.Size(579, 256);
             this.mechBox.TabIndex = 7;
             this.mechBox.TabStop = false;
@@ -88,7 +88,7 @@
             this.button1.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.857143F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.button1.Location = new System.Drawing.Point(25, 160);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(215, 53);
             this.button1.TabIndex = 14;
@@ -100,7 +100,7 @@
             // 
             this.listMechBox.ItemHeight = 19;
             this.listMechBox.Location = new System.Drawing.Point(260, 46);
-            this.listMechBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listMechBox.Margin = new System.Windows.Forms.Padding(2);
             this.listMechBox.Name = "listMechBox";
             this.listMechBox.Size = new System.Drawing.Size(295, 99);
             this.listMechBox.TabIndex = 15;
@@ -108,11 +108,8 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "1",
-            "2"});
             this.comboBox1.Location = new System.Drawing.Point(141, 44);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(83, 27);
             this.comboBox1.TabIndex = 12;
@@ -124,7 +121,7 @@
             this.AddMechanic.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.857143F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.AddMechanic.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.AddMechanic.Location = new System.Drawing.Point(340, 160);
-            this.AddMechanic.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.AddMechanic.Margin = new System.Windows.Forms.Padding(2);
             this.AddMechanic.Name = "AddMechanic";
             this.AddMechanic.Size = new System.Drawing.Size(215, 53);
             this.AddMechanic.TabIndex = 6;
@@ -395,7 +392,7 @@
             this.Controls.Add(this.mechBox);
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "RemoveMechanic";
             this.Text = "RemoveMechanic";
             this.Load += new System.EventHandler(this.RemoveMechanic_Load);
--- CarServiceAdministration/RemoveMechanic.cs
+++ CarServiceAdministration/RemoveMechanic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CarServiceAdministration.DBConnect;
+using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,62 +22,105 @@ namespace CarServiceAdministration
 
         private void RemoveMechanic_Load(object sender, EventArgs e)
         {
+            using (OracleConnection con = new OracleConnection(Database.connectionString))
+            {
+                try
+                {
+                    con.Open();
 
+                    string query = "SELECT MechID, Name, PhoneNum FROM Mechanics";
+
+                    using (OracleCommand cmd = new OracleCommand(query, con))
+                    {
+                        DataTable dt = new DataTable();
+                        dt.Load(cmd.ExecuteReader());
+
+                        comboBox1.DataSource = dt;
+                        comboBox1.DisplayMember = "MechID";
+                        comboBox1.ValueMember = "MechID";
+                        comboBox1.SelectedIndex = -1;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading customers: " + ex.Message);
+                }
+            }
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (comboBox1.SelectedIndex == 0)
-            {
-                listMechBox.Items.Clear();
+            if (comboBox1.SelectedItem == null || !(comboBox1.SelectedItem is DataRowView row))
+                return;
 
-                listMechBox.Items.Add("Mechanic Id: 1");
-                listMechBox.Items.Add("Mechanic Name: Alexander Kravintski");
-                listMechBox.Items.Add("Phone Number: 0876712698");
-            }
-            else if (comboBox1.SelectedIndex == 1)
-            {
-                listMechBox.Items.Clear();
+            listMechBox.Items.Clear();
 
-                listMechBox.Items.Add("Mechanic Id: 2");
-                listMechBox.Items.Add("Mechanic Name: John Doe");
-                listMechBox.Items.Add("Phone Number: 0876912678");
-            }
+            listMechBox.Items.Add("Mechanic Id: " + row["MechID"]);
+            listMechBox.Items.Add("Mechanic Name: " + row["Name"]);
+            listMechBox.Items.Add("Phone Number: " + row["PhoneNum"]);
         }
 
         private void AddMechanic_Click(object sender, EventArgs e)
         {
-            var confirmResult = MessageBox.Show("Are you sure you want to remove data?",
-                                     "Confirmation",
-                                     MessageBoxButtons.YesNo);
-            if (confirmResult == DialogResult.Yes)
-            {
-                MessageBox.Show("Mechanic Removed Successfully");
-                comboBox1.Items.Remove(comboBox1.SelectedItem);
-                listMechBox.Items.Clear();
-                comboBox1.Focus();
-            }
-            else
-            {
+            var confirmResult = MessageBox.Show(
+                "Are you sure you want to remove data?",
+                "Confirmation",
+                MessageBoxButtons.YesNo);
+
+            if (confirmResult != DialogResult.Yes)
                 return;
+
+            if (comboBox1.SelectedItem == null || !(comboBox1.SelectedItem is DataRowView row))
+            {
+                MessageBox.Show("Please select a valid Mechanic ID.");
+                return;
+            }
+
+            int mechId = Convert.ToInt32(row["MechID"]);
+
+            using (OracleConnection con = new OracleConnection(Database.connectionString))
+            {
+                try
+                {
+                    con.Open();
+
+                    string query = "DELETE FROM Mechanics WHERE MechID = :id";
+
+                    using (OracleCommand cmd = new OracleCommand(query, con))
+                    {
+                        cmd.Parameters.Add(":id", OracleDbType.Int32).Value = mechId;
+
+                        int result = cmd.ExecuteNonQuery();
+
+                        if (result > 0)
+                            MessageBox.Show("Mechanic Removed Successfully");
+                        else
+                            MessageBox.Show("Mechanic not found.");
+                    }
+
+                    listMechBox.Items.Clear();
+                    comboBox1.SelectedIndex = -1;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Database Error: " + ex.Message);
+                }
             }
         }
 
+
         private void button1_Click(object sender, EventArgs e)
         {
-            var confirmResult = MessageBox.Show("Are you sure you want to cancel?",
-                         "Confirmation",
-                         MessageBoxButtons.YesNo);
-            if (confirmResult == DialogResult.Yes)
-            {
-                this.Controls.Clear();
-                this.InitializeComponent();
-                comboBox1.Focus();
-            }
-            else
-            {
+            var confirmResult = MessageBox.Show(
+                           "Are you sure you want to cancel?",
+                           "Confirmation",
+                           MessageBoxButtons.YesNo);
+
+            if (confirmResult != DialogResult.Yes)
                 return;
-            }
+
+            comboBox1.SelectedIndex = -1;
+            listMechBox.Items.Clear();
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
